28a0b50faf42a8fbde0aa8c149c70fda42f21d10
David Hans <david.hans@sea.gob.cl>
2019-02-27 09:30:25 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var header = document.createElement('h1');
+var headerText = document.createTextNode('Hello World!');
+header.appendChild(headerText);
+
+document.getElementById('root').appendChild(header);
