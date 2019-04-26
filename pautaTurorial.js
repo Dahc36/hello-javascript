ac8d57e566bb15dc36a2e330b1afb7d4de8b8dbd
David Alberto Hans Castillo <dhans@sea.gob.cl>
2019-04-26 11:25:51 -0400

    ADD: archivos tutorial

---
+++ pautaTurorial.js
@@ -0,0 +1,98 @@
+// Project setup
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Tutorial JS</title>
+</head>
+<body>
+  <div>Hello JavaScript</div>
+</body>
+</html>
+
+// LLamar script
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Tutorial JS</title>
+</head>
+<body>
+  <div>Hello JavaScript</div>
+  <script src="index.js" ></script>
+</body>
+</html>
+
+// Runtime environment o ambiente de ejecución
+// 1 + 2
+// console.log('Hola mundo');
+
+// Console.log, hello world
+console.log('Hello world!');
+
+// 01 - Variables *****************************************************************************
+// Declaración
+var x = 1;
+console.log('X:');
+console.log(x);
+
+// Weakly typed
+var numeroUno = 1;
+console.log('Número:');
+console.log(x);
+// <meta charset="UTF-8">
+
+numeroUno = '1';
+console.log('Número:');
+console.log(x);
+
+// Primitive data types
+
+// cohercion
+// 1 + 1
+// '1' + 1
+// 1 + true
+// '' + 1 + true
+// 1 + true + ''
+// 1 + undefined
+var numeroUno = 1;
+console.log('Número: ' + numeroUno);
+
+// Hoisting let & const
+console.log('Número: ' + numeroUno);
+var numeroUno = 1;
+
+// Functions
+// Declaration, expression, named, anonymous
+// TODO: después de un error no se ha caído (se sigue ejecutando el ambiente)
+
+// Objects, arrays, functions
+// Spread operator
+// Scope & closure
+// Callbacks & Functional programming
+// High order functions, curried functions
+// For loop example
+// Objects
+// ejemplo cambiar un objeto, aunque sea declarado con const
+// comparison by value or reference
+// Constructor
+// Properties and methods
+// This, arrow func?
+// The prototype chain & static methods
+// Classes
+// Inheritance
+// Global objects & methods
+// String, template literals
+// Array: find, indexOf, filter, reduce, map, spread operator
+// Function: bind, call, apply
+// Object: keys, values, entries, spread operator
+// Math
+// Window, document
+// DOM manipulation
+// Hello world
+// Events
+// Async programming
+// Timeouts
+// Promises & event loop
+// Async await
+// Fetch
+// DOM manipulation example
+// Node project
