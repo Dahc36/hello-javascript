abf9609159ec1e64d2a9c42cf026340742e8368e
David Alberto Hans Castillo <dhans@sea.gob.cl>
2019-04-26 17:04:11 -0400

    ADD: avances tutorial, funciones

--- pautaTurorial.js
+++ pautaTurorial.js
@@ -28,7 +28,7 @@
 // Console.log, hello world
 console.log('Hello world!');
 
-// 01 - Variables *****************************************************************************
+// 01 - Variables ******************************************************************************
 // Declaración
 var x = 1;
 console.log('X:');
@@ -60,25 +60,145 @@ console.log('Número: ' + numeroUno);
 console.log('Número: ' + numeroUno);
 var numeroUno = 1;
 
-// Functions
-// Declaration, expression, named, anonymous
-// TODO: después de un error no se ha caído (se sigue ejecutando el ambiente)
+console.log('1 - Número: ' + numeroUno);
+var numeroUno = 1;
+console.log('2 - Número: ' + numeroUno);
 
-// Objects, arrays, functions
-// Spread operator
-// Scope & closure
-// Callbacks & Functional programming
-// High order functions, curried functions
-// For loop example
+var numeroUno
+console.log('1 - Número: ' + numeroUno);
+numeroUno = 1;
+console.log('2 - Número: ' + numeroUno);
+
+console.log('1 - Número: ' + numeroUno);
+console.log('2 - Número 2: ' + numeroDos);
+var numeroUno = 1;
+console.log('3 - Número: ' + numeroUno);
+// numeroUno
+// numeroDos
+
+console.log('1 - Número: ' + numeroUno);
+console.log('2 - Número2: ' + numeroDos);
+var numeroUno = 1;
+console.log('3 - Número: ' + numeroUno);
+var numeroDos = 2;
+// numeroUno
+// numeroDos
+
+console.log('1 - Número: ' + numeroUno);
+console.log('2 - Número2: ' + numeroDos);
+var numeroUno = 1;
+console.log('3 - Número: ' + numeroUno);
+let numeroDos = 2;
+// Temporal dead zone
+// let let
+
+console.log('1 - Número: ' + numeroUno);
+let numeroDos;
+console.log('2 - Número2: ' + numeroDos);
+var numeroUno = 1;
+console.log('3 - Número: ' + numeroUno);
+numeroDos = 2;
+
+console.log('1 - Número: ' + numeroUno);
+let numeroDos = 2;
+console.log('2 - Número2: ' + numeroDos);
+var numeroUno = 1;
+console.log('3 - Número: ' + numeroUno);
+numeroDos = 'a';
+
+console.log('1 - Número: ' + numeroUno);
+const numeroDos = 2;
+console.log('2 - Número2: ' + numeroDos);
+var numeroUno = 1;
+console.log('3 - Número: ' + numeroUno);
+numeroDos = 'a';
+
+var numeroPar = 1;
+if (numeroPar % 2 == 1) {
+  var numeroPar = 2;
+  console.log('1 - Número: ' + numeroPar);
+}
+console.log('2 - Número ' + numeroPar);
+
+// 02 - Functions ******************************************************************************
+// Declaration, expression, named, anonymous
+function nombreFunc(arg) {
+  console.log('nombreFunc');
+  return arg;
+}
+console.log('Returns: ' + nombreFunc());
+
+const nombreFunc = function(arg) {
+  console.log('nombreFunc');
+  return arg;
+}
+console.log('Returns: ' + nombreFunc());
+
+// Hoisting
+var x = 1;
+console.log(x); // 1
+console.log(y); // undefined
+console.log(z); // Uncaught ReferenceError: z is not defined
+var y = 2;
+
+function x() { return 1 };
+console.log(x());
+console.log(y());
+console.log(z());
+function y() { return 2 };
+var z = function() { return 3 };
+
+// Argumentos pasados por valor
+let x = 1;
+let func = function(x) {
+  x = x + 3;
+  return x;
+}
+console.log(func(x));
+console.log(x);
+
+// Closure
+let x = 1
+const func1 = function(y) {
+  x = x + y;
+};
+func1(2);
+console.log(x);
+
 // Objects
 // ejemplo cambiar un objeto, aunque sea declarado con const
+let myObj = {
+  x: 1,
+  y: 2
+};
+console.log('X: ' + myObj.x);
+console.log('Y: ' + myObj.y);
+console.log('Obj: ' + myObj);
+
+
+
+// Arrays, functions
+
+
+// Spread operator
+// For loop example
 // comparison by value or reference
 // Constructor
 // Properties and methods
-// This, arrow func?
 // The prototype chain & static methods
 // Classes
 // Inheritance
+// This, arrow func?
+
+// Callbacks & Functional programming
+const add = function(x) {
+  return function(y) {
+    return x + y;
+  }
+};
+
+// High order functions, curried functions
+
 // Global objects & methods
 // String, template literals
 // Array: find, indexOf, filter, reduce, map, spread operator
---
+++ tutorial.js
@@ -0,0 +1,6 @@
+let myObj = {
+  x: 1,
+  y: 2
+};
+myObj.z = 3;
+console.log(myObj);
