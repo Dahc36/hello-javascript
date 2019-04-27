3b120333c6105f6dcb24802fe5a792f03b0f2fe1
Hans, David <dahc36@gmail.com>
2019-04-26 23:45:16 -0400

    ADD: objects, tutorial

--- pautaTurorial.js
+++ pautaTurorial.js
@@ -165,39 +165,183 @@ const func1 = function(y) {
 func1(2);
 console.log(x);
 
-// Objects
-// ejemplo cambiar un objeto, aunque sea declarado con const
+// 03 - Objects ******************************************************************************
+// Properties and methods
+let propName = 'String value';
 let myObj = {
   x: 1,
-  y: 2
+  y: 2,
+  foo: function(arg) {
+    return arg;
+  },
+  [proName]: 'I\'m a string'
 };
-console.log('X: ' + myObj.x);
-console.log('Y: ' + myObj.y);
-console.log('Obj: ' + myObj);
+myObj.z = 3;
+myObj['a'] = 'a';
+console.log(myObj);
 
-
-
-// Arrays, functions
-
+// Referencia
+// comparison by value or reference
+let obj1 = {
+  x: 1
+};
+let obj2 = {
+  x: 1
+};
+console.log(obj1 == obj2);
+console.log(obj1 === obj2);
 
 // Spread operator
-// For loop example
-// comparison by value or reference
-// Constructor
-// Properties and methods
-// The prototype chain & static methods
-// Classes
-// Inheritance
-// This, arrow func?
+let obj1 = {
+  x: 1
+};
+let obj2 = obj1;
+obj1.x = 2;
+console.log(obj1);
+console.log(obj2);
 
-// Callbacks & Functional programming
-const add = function(x) {
-  return function(y) {
-    return x + y;
+// Arrays
+let arr1 = [ 1, 2, 3, 4 ];
+let arr2 = [ ...arr1 ];
+arr1[1] = 'Hello';
+console.log(arr1);
+console.log(arr2);
+
+// Ejemplo cambiar un objeto, aunque sea declarado con const
+const obj = {
+  x: 1
+};
+obj = {
+  x: 2
+};
+console.log(obj);
+
+const obj = {
+  x: 1
+};
+obj.x = 2;
+console.log(obj);
+
+// This, arrow func?
+// this -> window
+// var x = 'a'; window.varX
+// let x = 'a'; window.letX
+const foo = function () {
+  return this;
+} 
+obj1 = {
+  a: 1,
+  bar: foo
+};
+obj2 = {
+  b: 2,
+  bar: foo
+};
+console.log(obj1.bar());
+console.log(obj2.bar());
+
+const foo = function () {
+  let that = this;
+  const foo2 = function () {
+    return that;
   }
+  return foo2();
+}
+obj1 = {
+  a: 1,
+  bar: foo
+};
+obj2 = {
+  b: 2,
+  bar: foo
+};
+console.log(obj1.bar());
+console.log(obj2.bar());
+
+const foo = function() {
+  const foo2 = () => {
+    return this;
+  }
+  return foo2();
+}
+obj1 = {
+  a: 1,
+  bar: foo
+};
+obj2 = {
+  b: 2,
+  bar: _ => this
 };
 
-// High order functions, curried functions
+console.log(obj1.bar());
+console.log(obj2.bar());
+
+const foo = function() {
+  const foo2 = () => {
+    return this;
+  };
+  return foo2;
+};
+obj1 = {
+  a: 1,
+  bar: foo
+};
+
+obj2 = {
+  b: 2,
+  bar: foo
+};
+const foo1 = obj1.bar();
+console.log(foo1());
+console.log(obj2.bar()());
+
+// Classes
+// Constructor
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+  area() {
+    return this.side * this.side;
+  }
+}
+const square = new Square();
+// The prototype chain (algo falta)
+
+// Inheritance
+class Animal {
+  constructor(species, legs) {
+    this.species = species;
+    this.legs = legs;
+  }
+  speak() {
+    return '...';
+  }
+}
+class Dog extends Animal {
+  constructor(breed) {
+    super('Mammal', 4);
+    this.breed = breed;
+  }
+
+  speak() {
+    return 'Woof!';
+  }
+}
+class Spider extends Animal {
+  constructor() {
+    super('Arachnid', 8);
+  }
+}
+const dog = new Dog('German shepherd');
+const spider = new Spider();
+
+// Static methods
+// ...
+  static isDog(animal) {
+    return animal.speak() === 'Woof!';
+  }
+// ...
 
 // Global objects & methods
 // String, template literals
@@ -206,6 +350,15 @@ const add = function(x) {
 // Object: keys, values, entries, spread operator
 // Math
 // Window, document
+
+// Callbacks & Functional programming
+const add = function(x) {
+  return function(y) {
+    return x + y;
+  }
+};
+// High order functions, curried functions
+
 // DOM manipulation
 // Hello world
 // Events
--- tutorial.js
+++ tutorial.js
@@ -1,6 +1,34 @@
-let myObj = {
-  x: 1,
-  y: 2
-};
-myObj.z = 3;
-console.log(myObj);
+class Animal {
+  constructor(species, legs) {
+    this.species = species;
+    this.legs = legs;
+  }
+
+  static isDog(animal) {
+    return animal.speak() === 'Woof!';
+  }
+
+  speak() {
+    return '...';
+  }
+}
+
+class Dog extends Animal {
+  constructor(breed) {
+    super('Mammal', 4);
+    this.breed = breed;
+  }
+
+  speak() {
+    return 'Woof!';
+  }
+}
+
+class Spider extends Animal {
+  constructor() {
+    super('Arachnid', 8);
+  }
+}
+
+const dog = new Dog('German shepherd');
+const spider = new Spider();
