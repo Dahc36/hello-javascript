4c7917fb2f974735e2e14f69ca687ccfa2aaba7f
Hans, David <dahc36@gmail.com>
2019-04-27 21:47:12 -0400

    ADD: avances, proE

--- helpers/projectEuler.js
+++ helpers/projectEuler.js
@@ -1,5 +1,5 @@
 // 1. Multiples of 3 and 5
-function multiplesOf3And5Below(n) {
+const multiplesOf3And5Below = function (n) {
   let sum = 0;
   for (let i = 0; i < n; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
@@ -8,10 +8,10 @@ function multiplesOf3And5Below(n) {
   }
 
   return sum;
-}
+};
 
 // 2. Even Fibonacci numbers
-function evenFibonacciBelow(n) {
+const evenFibonacciBelow = function (n) {
   let fibNumbers = [ null, 1, 2 ];
   let index = 3;
   let sum = 2;
@@ -25,10 +25,10 @@ function evenFibonacciBelow(n) {
     }
     index += 1;
   }
-}
+};
 
 // 3. Largest prime factor
-function largestPrimeFactorBelow(n) {
+const largestPrimeFactorBelow = function (n) {
   let index = 2;
   if (isPrime(n)) {
     return null;
@@ -41,9 +41,9 @@ function largestPrimeFactorBelow(n) {
   }
 
   return null;
-}
+};
 
-function isPrime(n) {
+const isPrime = function (n) {
   if (n < 4) {
     return n === 2 || n === 3;
   }
@@ -52,7 +52,7 @@ function isPrime(n) {
   }
 
   let i = 5;
-  while (i * i < n) {
+  while (i * i <= n) {
     if (n % i === 0) {
       return false
     }
@@ -61,4 +61,197 @@ function isPrime(n) {
   }
 
   return true;
+};
+
+// 4. Largest palindrome product
+const largestPalindromeProduct = function (digits) {
+  const n = Number.parseInt(Array(digits).fill(9).join(''));
+  const limit = Number.parseInt(Array(digits - 1).fill(9).join(''));
+  let result = -1;
+  for (let i = n; i > limit; i--) {
+    for (let j = i; j > limit; j--) {
+      if (i ** i < result) {
+        return result;
+      }
+
+      const prod = i * j;
+      if (prod > result && isPalindrome(prod)) {
+        result = prod;
+      }
+    }
+  }
+  return result;
+};
+
+const isPalindrome = function (digit) {
+  const str = digit.toString();
+  const len = str.length;
+  let i = 0;
+  while (i < len) {
+    if (str.charAt(i) !== str.charAt(len - 1 - i)) {
+      return false;
+    }
+    i += 1;
+  }
+
+  return true;
+};
+
+// 5. Smallest multiple
+const smallestMultiple = function (limit) {
+  let i = limit + 1;
+  while (true) {
+    let flag = true;
+    for (let j = 2; j < limit + 1; j++) {
+      if (i % j !== 0) {
+        flag = false
+        break;
+      }
+    }
+
+    if (flag) {
+      return i;
+    }
+    i++;
+  }
+};
+
+// 6. Sum square difference
+const sumSquareDifference = function (n) {
+  let sum = 0;
+  let squareSum = 0;
+  for (let i = 1; i < n + 1; i++) {
+    sum += i;
+    squareSum += i ** 2;
+  }
+
+  return sum ** 2 -squareSum;
+};
+
+// 7. 10001st prime
+const primeNumber = function (n) {
+  if(!n) {
+    return null;
+  }
+  if (n === 1) {
+    return 2;
+  }
+  if (n === 2) {
+    return 3;
+  }
+
+  let count = 2;
+  let i = 3;
+  while (count < n) {
+    i += 1;
+    if (isPrime(i)) {
+      count++;
+    }
+  }
+
+  return i;
+};
+
+// 8. Largest product in a series
+const largestProductInSeries = function (series, digits = 2) {
+  const arr = series.split('');
+  let products = [];
+  let max = 0;
+  for (let i = 0; i < arr.length - digits + 1; i++) {
+    let prod = arr[i];
+    for (let j = 1; j < digits; j++) {
+      prod *= arr[i + j]
+    }
+
+    if (prod > max) {
+      max = prod;
+    }
+  }
+
+  return max;
+};
+
+// 9. Special Pythagorean triplet
+const specialPythagoreanTriplet = function (n) {
+  const max = Math.pow(n, 1 / 2);
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < n - i; j++) {
+      for (let k = 1; k < i + j; k++) {
+        if (i + j + k === n && i ** 2 + j ** 2 === k ** 2) {
+          return i * j * k;
+        }
+      }
+    }
+  }
+};
+
+// 10. Summation of primes
+const summationPrimesBelow = function (n) {
+  if(!n) {
+    return null;
+  }
+
+  let sum = 0;
+  let i = 2;
+  while (i < n - 1) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+    i += 1;
+  }
+
+  return sum;
+};
+
+// 11. Largest product in a grid
+const largestProductGrid = function (strGrid, digits = 2) {
+  const grid = strGrid
+    .split('\n')
+    .map(value => value.split(' '));
+  let maxProd = 0;
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      if (col + digits - 1 < grid[row].length) {
+        let tempMaxProd = grid[row][col];
+        for (let i = 1; i < digits; i++) {
+          tempMaxProd *= grid[row][col + i];
+        }
+        if (tempMaxProd > maxProd) {
+          maxProd = tempMaxProd;
+        }
+
+        if (row + digits - 1 < grid.length) {
+          tempMaxProd = grid[row][col];
+          for (let i = 1; i < digits; i++) {
+            tempMaxProd *= grid[row + i][col + i];
+          }
+          if (tempMaxProd > maxProd) {
+            maxProd = tempMaxProd;
+          }
+        }
+      }
+
+      if (row + digits - 1 < grid.length) {
+        let tempMaxProd = grid[row][col];
+        for (let i = 1; i < digits; i++) {
+          tempMaxProd *= grid[row + i][col];
+        }
+        if (tempMaxProd > maxProd) {
+          maxProd = tempMaxProd;
+        }
+
+        if (col - digits + 1 > - 1) {
+          tempMaxProd = grid[row][col];
+          for (let i = 1; i < digits; i++) {
+            tempMaxProd *= grid[row + i][col - i];
+          }
+          if (tempMaxProd > maxProd) {
+            maxProd = tempMaxProd;
+          }
+        }
+      }
+    }
+  }
+  return maxProd;
 }
+
